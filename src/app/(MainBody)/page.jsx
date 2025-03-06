51b1503ff2f22d220e8eecb33e7fa5385aaec8b5
nugo dev <info@nugounlimited.com>
2025-03-05 20:48:10 -0500

    fingers crossed

--- src/app/(MainBody)/page.jsx
+++ src/app/(MainBody)/page.jsx
@@ -1,39 +1,27 @@
-"use client";
-import { useEffect } from "react";
-import BlogSection from "../../containers/landing/blog";
-import DemoSection from "../../containers/landing/demo";
-import ElementSection from "../../containers/landing/elements";
-import FeaturesSection from "../../containers/landing/features";
-import FooterSection from "../../containers/landing/footer";
-import GiftSection from "../../containers/landing/git-section";
-import Header from "../../containers/landing/header";
-import PortfolioSection from "../../containers/landing/portfolio";
-import RatSection from "../../containers/landing/rat";
-import TestimonialSection from "../../containers/landing/testimonial";
-import WhatinsideSection from "../../containers/landing/whatinside";
+// containers/landing/page.jsx
+import React, { useEffect } from "react";
 
-const Portfolio = () => {
+// Import gym-related sections, excluding the ones you don't need
+import GymBanner from "../../containers/layouts/sections/gym/banner";
+import GymAbout from "../../containers/layouts/sections/gym/about";
+import GymTestimonial from "../../containers/layouts/sections/gym/testimonial";
+import GymFooter from "../../containers/layouts/sections/gym/footer";
+
+const GymLandingPage = () => {
   useEffect(() => {
-    document.body.style.setProperty("--primary", "#10266b");
+    document.body.style.setProperty("--primary", "#10266b"); // Customize colors
     document.body.style.setProperty("--secondary", "#464545");
     document.body.style.setProperty("--light", "#1F357D");
-    document.body.style.setProperty("--dark", "#04185B");
-  });
+  }, []);
+
   return (
-    <div className="landing-page">
-      <Header />
-      <GiftSection />
-      <DemoSection />
-      <PortfolioSection />
-      <WhatinsideSection />
-      <BlogSection />
-      <ElementSection />
-      <FeaturesSection />
-      <TestimonialSection />
-      <RatSection />
-      <FooterSection />
-    </div>
+    <>
+      <GymBanner />
+      <GymAbout />
+      <GymTestimonial />
+      <GymFooter />
+    </>
   );
 };
 
-export default Portfolio;
+export default GymLandingPage;
--- src/containers/landing/header.js
+++ src/containers/landing/header.js
@@ -1,61 +1,52 @@
-import React, { Fragment, useState } from 'react';
-import { Container, Row, Col } from 'reactstrap'
+// containers/landing/header.js
+import React, { Fragment, useState } from "react";
+import { Container, Row, Col } from "reactstrap";
+
 const Header = () => {
+  const [navbar, setNavbar] = useState(false);
+  const toggleNav = () => {
+    setNavbar(!navbar);
+  };
 
-    const [navbar, setNavbar] = useState(false);
-    const toggleNav = () => {
-        setNavbar(!navbar)
-    }
-    return (
-
-        <Fragment>
-            <header>
-                <Container fluid={true}>
-                    <Row>
-                        <Col>
-                            <div className="top-header">
-                                <div className="logo">
-                                    <a className="navbar-brand" href="#"><img src="assets/images/landing/logo.png"
-                                        alt="logo" /></a>
-                                </div>
-                                <div className="main-menu m-x-auto" id="nav">
-                                    <nav id="navbar-example2" className="navbar navbar-expand-lg navbar-light">
-                                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-target="#scroll-spy"
-                                            aria-controls="scroll-spy" aria-expanded="false" aria-label="Toggle navigation" onClick={toggleNav}>
-                                            <span className="navbar-toggler-icon"></span>
-                                        </button>
-                                        <div className={`collapse navbar-collapse ${navbar === true ? 'show' : ''}`} id="scroll-spy">
-                                            <ul className="navbar-nav mx-auto nav">
-                                                <li className="nav-item">
-                                                    <a className="nav-link active" href="#home">Home</a>
-                                                </li>
-                                                <li className="nav-item">
-                                                    <a className="nav-link" href="#pages">pages</a>
-                                                </li>
-                                                <li className="nav-item">
-                                                    <a className="nav-link" href="#portfolio">portfolio</a>
-                                                </li>
-                                                <li className="nav-item">
-                                                    <a className="nav-link" href="#feature">features</a>
-                                                </li>
-                                                <li className="nav-item">
-                                                    <a className="nav-link" href="https://pixelstrap.freshdesk.com">support</a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </nav>
-                                </div>
-                                <div className="purchase-block">
-                                    <span className="cartpurchase"><i className="fa fa-cart-arrow-down"></i></span>
-                                    <a className="purchase-btn" href="https://themeforest.net/user/pixelstrap/portfolio">purchase</a>
-                                </div>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </header>
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <header>
+        <Container fluid={true}>
+          <Row>
+            <Col>
+              <div className="top-header">
+                <div className="logo">
+                  <a className="navbar-brand" href="/">
+                    <img src="assets/images/landing/logo.png" alt="Gym Logo" />
+                  </a>
+                </div>
+                <div className="main-menu m-x-auto" id="nav">
+                  <nav className="navbar navbar-expand-lg navbar-light">
+                    <button className="navbar-toggler" type="button" onClick={toggleNav}>
+                      <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className={`collapse navbar-collapse ${navbar ? "show" : ""}`} id="navbarNav">
+                      <ul className="navbar-nav">
+                        <li className="nav-item">
+                          <a className="nav-link" href="/">Home</a>
+                        </li>
+                        <li className="nav-item">
+                          <a className="nav-link" href="#about">About</a>
+                        </li>
+                        <li className="nav-item">
+                          <a className="nav-link" href="#contact">Contact</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </nav>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </header>
+    </Fragment>
+  );
+};
 
 export default Header;
