07255b7015e8e4bbefb7f641c3b4a420fc4c4fe2
nugo dev <info@nugounlimited.com>
2025-03-05 20:31:23 -0500

    trim landing page

--- src/app/(MainBody)/layouts/gym/page.js
+++ src/app/(MainBody)/layouts/gym/page.js
@@ -6,7 +6,7 @@ import Header from "../../../../containers/common/header";
 import AboutSection from "../../../../containers/layouts/sections/gym/about";
 import BannerSection from "../../../../containers/layouts/sections/gym/banner";
 import BrandSection from "../../../../containers/layouts/sections/gym/brand";
-//import CalculateSection from "../../../../containers/layouts/sections/gym/calculate";
+import CalculateSection from "../../../../containers/layouts/sections/gym/calculate";
 import CounterSection from "../../../../containers/layouts/sections/gym/counter";
 import FooterSection from "../../../../containers/layouts/sections/gym/footer";
 import FormatSection from "../../../../containers/layouts/sections/gym/format";
